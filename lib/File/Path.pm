da130485fba8a649818c53703f412be0be5bff4d
Richard Elberger <elberger@amazon.com>
2017-03-12 18:06:50 -0400

    Add changes to dev release, bump version

--- lib/File/Path.pm
+++ lib/File/Path.pm
@@ -18,7 +18,7 @@ BEGIN {
 
 use Exporter ();
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
-$VERSION   = '2.12_001';
+$VERSION   = '2.12_002';
 $VERSION   = eval $VERSION;
 @ISA       = qw(Exporter);
 @EXPORT    = qw(mkpath rmtree);
