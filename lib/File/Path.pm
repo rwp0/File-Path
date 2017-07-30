965f9c2644e29cb62c8a35bb22df9fa61f596184
James E Keenan <jkeenan@cpan.org>
2017-07-29 22:28:03 -0400

    Remove erroneous documentation.
    Files and symlinks can be provided as arguments for remove_tree() and rmtree()
    -- though that is not the functions' intent.
    
    For: https://rt.cpan.org/Public/Bug/Display.html?id=122648

--- lib/File/Path.pm
+++ lib/File/Path.pm
@@ -774,12 +774,14 @@ identical to C<make_path()>.
 
 The C<remove_tree> function deletes the given directories and any
 files and subdirectories they might contain, much like the Unix
-command C<rm -rf> or the Windows commands C<rmdir /s> and C<rd /s>. The
-only exception to the function similarity is that C<remove_tree> accepts
-only directories whereas C<rm -rf> also accepts files.
+command C<rm -rf> or the Windows commands C<rmdir /s> and C<rd /s>.
 
-The function accepts a list of directories to be
-removed. Its behaviour may be tuned by an optional hashref
+The function accepts a list of directories to be removed. (In point of fact,
+it will also accept filesystem entries which are not directories, such as
+regular files and symlinks.  But, as its name suggests, its intent is to
+remove trees rather than individual files.)
+
+C<remove_tree()>'s behaviour may be tuned by an optional hashref
 appearing as the last parameter on the call.  If an empty string is
 passed to C<remove_tree>, an error will occur.
 
