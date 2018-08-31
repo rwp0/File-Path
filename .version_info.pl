42c0fb8b422ee97e73a5f530c07813ad6259c859
James E Keenan <jkeenan@cpan.org>
2018-08-31 08:36:44 -0400

    CPAN version 2.16
    Update version numbers and Changes.

--- .version_info.pl
+++ .version_info.pl
@@ -1,8 +1,8 @@
 # .version_info.pl for File-Path
 %trans = (
-    version => { old => '2.14', new => '2.15' },
-    date    => { old => 'June 07 2017', new => 'July 29 2017' },
-    copyright => { old => '2016', new => '2017' },
+    version => { old => '2.15', new => '2.16' },
+    date    => { old => 'July 29 2017', new => 'August 31 2018' },
+    copyright => { old => '2017', new => '2018' },
 );
 
 1;
