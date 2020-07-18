194e4e9e6fe32a15cc6efbd5a3154da1101a5b09
James E Keenan <jkeenan@cpan.org>
2020-07-18 14:20:12 -0400

    Bump $VERSION, copyright, release date

--- .version_info.pl
+++ .version_info.pl
@@ -1,8 +1,8 @@
 # .version_info.pl for File-Path
 %trans = (
-    version => { old => '2.15', new => '2.16' },
-    date    => { old => 'July 29 2017', new => 'August 31 2018' },
-    copyright => { old => '2017', new => '2018' },
+    version => { old => '2.16', new => '2.17' },
+    date    => { old => 'August 31 2018', new => 'July 18 2020' },
+    copyright => { old => '2018', new => '2020' },
 );
 
 1;
