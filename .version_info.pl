08ac7c22344ec65b66105f781254bcbe471e17c4
James E Keenan <jkeenan@cpan.org>
2020-11-04 20:26:57 -0500

    CPAN release 2.18

--- .version_info.pl
+++ .version_info.pl
@@ -1,7 +1,7 @@
 # .version_info.pl for File-Path
 %trans = (
-    version => { old => '2.17', new => '2.18_001' },
-    date    => { old => 'July 18 2020', new => 'November 4 2020' },
+    version => { old => '2.18_001', new => '2.18' },
+    date    => { old => 'November 4 2020', new => 'November 4 2020' },
     copyright => { old => '2020', new => '2020' },
 );
 
--- lib/File/Path.pm
+++ lib/File/Path.pm
@@ -18,7 +18,7 @@ BEGIN {
 
 use Exporter ();
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
-$VERSION   = '2.18_001';
+$VERSION   = '2.18';
 $VERSION   = eval $VERSION;
 @ISA       = qw(Exporter);
 @EXPORT    = qw(mkpath rmtree);
@@ -633,7 +633,7 @@ File::Path - Create or remove directory trees
 
 =head1 VERSION
 
-2.18_001 - released November 4 2020.
+2.18 - released November 4 2020.
 
 =head1 SYNOPSIS
 
